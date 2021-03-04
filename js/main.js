66b85690936675f3dec3baaa6b770795c2c89424
yimsoeun <78005307+yimsoeun@users.noreply.github.com>
2021-03-04 16:29:54 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+//(function() {}) (); 동일
+(() => {
+
+	let yOffset = 0; //window.pageYOffset 대신 쓰는 변수
+	let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이 합
+	let currentScene = 0; //현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
+
+	const sceneInfo = [
+		{
+			//0
+			type: 'sticky',
+			heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+			scrollHeight: 0, //스크롤 높이
+			objs: {
+				container: document.querySelector('#scroll-section-0')
+			}
+		},
+		{
+			//1
+			type: 'normal',
+			heightNum: 5, 
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-1')
+			}
+		},
+		{	//2
+			type: 'sticky',
+			heightNum: 5, 
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-2')
+			}
+		},
+		{	//3
+			type: 'sticky',
+			heightNum: 5, 
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-3')
+			}
+		}
+	];
+
+	function setLayout(){
+		//각 스크롤 섹션의 높이 세팅
+		for(let i = 0; i< sceneInfo.length; i++){
+			sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+			sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`; // 기본적으로 문자열이지만 달러의 중괄호 내부에는 변수를 쓸 수 있다.
+		}
+	};
+	
+	function scrollLoop(){
+		prevScrollHeight = 0;
+		for(let i = 0; i < currentScene; i++){
+			prevScrollHeight += sceneInfo[i].scrollHeight;
+			
+		}
+
+		if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+			currentScene++;
+		}
+
+		if(yOffset < prevScrollHeight){
+			if(currentScene === 0) return; //브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+			currentScene--;
+		}
+
+		document.body.setAttribute('id', `show-scene-${currentScene}`);
+	}
+
+	window.addEventListener('resize', setLayout);
+	window.addEventListener('scroll', () =>{
+		yOffset = window.pageYOffset;
+		scrollLoop();
+	});
+	setLayout();
+
+})();
